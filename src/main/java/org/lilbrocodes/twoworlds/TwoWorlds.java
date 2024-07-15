6df56550e82d111bc666bc6f93b921614c11d0b8
LilBroCodes <lilbrocodes@gmail.com>
2024-07-16 00:35:20 +0200

    1.1

--- src/main/java/org/lilbrocodes/twoworlds/TwoWorlds.java
+++ src/main/java/org/lilbrocodes/twoworlds/TwoWorlds.java
@@ -28,7 +28,7 @@ public final class TwoWorlds extends JavaPlugin {
         String sNeonPurple = ChatColor.translateAlternateColorCodes('&', "&x&a&8&0&1&e&9");         // #a801e9
         String neonBlue = ChatColor.translateAlternateColorCodes('&', "&x&1&f&5&1&f&f");            // #1F51FF
         String sNeonBlue = ChatColor.translateAlternateColorCodes('&', "&x&4&6&6&f&f&f");           // #466fff
-        String white = ChatColor.translateAlternateColorCodes('&', "&x&f&f&f&f&f&f");               // #FFFFFF
+        String white = ChatColor.translateAlternateColorCodes('&', "&x&f&f&f&f&f&f");// #FFFFFF
 
         console.sendMessage("");
         console.sendMessage(neonPurple +  "                   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░                      ");
@@ -38,7 +38,7 @@ public final class TwoWorlds extends JavaPlugin {
         console.sendMessage(neonPurple +  "                      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░                     ");
         console.sendMessage(neonPurple +  "                      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░                     ");
         console.sendMessage(neonPurple +  "                      ░▒▓█▓▒░    ░▒▓█████████████▓▒░ ░▒▓██████▓▒░                      ");
-        console.sendMessage(sNeonPurple + "                                 Enabling Two Worlds                                   ");
+        console.sendMessage(sNeonPurple + "                               Enabling Two Worlds v1.1                                ");
         console.sendMessage(white +       "███████████████████████████████████████████████████████████████████████████████████████");
         console.sendMessage(sNeonBlue +   "                              Made with ♥ by LilBroCodes                               ");
         console.sendMessage(neonBlue +    "░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓███████▓▒░ ");
